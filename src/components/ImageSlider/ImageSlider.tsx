8b869669bb0d1c0ab98cdd1261501d59fb641572
Dan Mills <danmills@Dans-MacBook-Pro.local>
2021-02-19 21:38:47 +0000

    Add custom css for ImageSlider

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -3,6 +3,7 @@ import SlickSlider, { Settings } from 'react-slick'
 import styled from 'styled-components'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
+import '../../css/index.css'
 
 export interface ImageSliderProps {
   urls: string[]
